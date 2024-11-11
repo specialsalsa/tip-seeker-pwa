526536d5833ff6d22eb82b7a8d04eba33778ed3e
specialsalsa <floopikiii@gmail.com>
2024-11-11 11:52:37 -0800

    tip rating stars

--- components/TipLogCard.vue
+++ components/TipLogCard.vue
@@ -4,21 +4,39 @@
     <v-card-subtitle>
       Average: {{ getTipRatingAverage(tipRatings) }}
     </v-card-subtitle>
+    <v-icon icon="mdi-star"></v-icon>
+    <v-icon
+      v-for="i in 4"
+      :icon="handleStarRating(average, i + 0.3, i + 0.7)"
+    ></v-icon>
   </v-card>
 </template>
 
 <script setup lang="ts">
-  defineProps<{
+  const props = defineProps<{
     title?: string;
     tipRatings: string[] | undefined;
   }>();
 
-  const getTipRatingAverage = (ratingArr: string[] | undefined) => {
-    if (!ratingArr) return "";
-    return (
+  const getTipRatingAverage = (ratingArr: string[] | undefined): number => {
+    if (!ratingArr) return 0;
+    return +(
       ratingArr.reduce((acc, cur) => (acc += parseInt(cur)), 0) /
       ratingArr.length
     ).toFixed(2);
+  };
+
+  const average = getTipRatingAverage(props.tipRatings);
+
+  const handleStarRating = (
+    rating: number,
+    low: number,
+    high: number
+  ): string => {
+    if (rating < low) return "mdi-star-outline";
+    else if (rating >= low && rating < high) return "mdi-star-half-full";
+    else if (rating >= high) return "mdi-star";
+    else return "mdi-star";
   };
 </script>
 
