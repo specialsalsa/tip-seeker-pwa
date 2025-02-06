115930329774cc1cc604757bd2b6305a89b5ba51
specialsalsa <floopikiii@gmail.com>
2025-02-05 21:48:02 -0800

    fix user key

--- app.vue
+++ app.vue
@@ -26,6 +26,7 @@
   import { useAuth } from "./composables/useAuth";
   import { useThemeSwitcher } from "./composables/useThemeSwitcher";
   import { useUserStore } from "./store/store";
+  import { getUserKey } from "./util/util";
   const { updateTheme, watchThemeChange } = useThemeSwitcher();
   const store = useUserStore();
 
@@ -33,6 +34,7 @@
 
   onMounted(async () => {
     await new Promise(async (resolve) => resolve(await useAuth()));
+    store.userKey = getUserKey();
     isLoading.value = false;
     updateTheme();
     watchThemeChange();
